36d757d7c8d62f2eba9ea1e34ae226e09fea2935
dalgatkadyrov <dalgatkadyrov243@gmail.com>
2026-09-15 14:56:18 -0400

    docker inclusion

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,9 @@
 import express from "express"
 import cors from 'cors'
+
 import "dotenv/config"
+import fs from 'fs'
+import path from 'path'
 
 import { clerkMiddleware } from '@clerk/express'
 
@@ -8,8 +11,11 @@ import User from './models/user.model.js'
 import { connectDB } from "./lib/db.js"
 
 const app = express()
+
 const PORT = process.env.PORT
 const FRONTEND_URL = process.env.FRONTEND_URL
+
+const publicDir = path.join(process.cwd(), 'public')
 
 app.use(express.json())
 app.use(cors({ origin: FRONTEND_URL, credentials: true }))
@@ -19,6 +25,16 @@ app.get('/health', (req, res) => {
 
     res.status(200).json({ ok: true })
 })
+
+//if public dir exists, serve the static file
+//its for production build
+if (fs.existsSync(publicDir)) {
+    app.use(express.static(publicDir))
+    
+    app.get('/{*any}', (req, res, next) => {
+        res.sendFile(path.join(publicDir, 'index.html'), (err) => next(err))
+    })
+}
 
 app.listen(PORT, () => {
     connectDB()
